50f042dd2f45afa66c737c12119cca6b4c8fe582
RobinMarien <dev.robinmarien@gmail.com>
2019-09-10 17:04:52 +0200

    average temp working

--- js/script.js
+++ js/script.js
@@ -48,67 +48,64 @@ document.getElementById("check").addEventListener("click", function(){
             var t2_img = document.getElementById("two");
             var t2_icon = data.list[0].weather[0].icon;
             */
+            var temp1_arr = [];
+            var temp2_arr = [];
+            var temp3_arr = [];
+            var temp4_arr = [];
+            var temp5_arr = [];
 
             var list = data.list;
 
             list.forEach(function(dataEveryThreeHours){
 
                 //console.log(dataEveryThreeHours);
-                var temp1 = [];
-                var minTemp1 = [];
-                var maxTemp1 = [];
 
-                var temp2 = [];
-                var minTemp2 = [];
-                var maxTemp2 = [];
-
-                var temp3 = [];
-                var minTemp3 = [];
-                var maxTemp3 = [];
-
-                var temp4 = [];
-                var minTemp4 = [];
-                var maxTemp4 = [];
-
-                var temp5 = [];
-                var minTemp5 = [];
-                var maxTemp5 = [];
 
                 var dateNumber = parseInt((dataEveryThreeHours.dt_txt).slice(8,10));
                 let today = new Date().getDate();
 
                 if (dateNumber == today){
-                    temp1.push(dataEveryThreeHours.main.temp);
-                    minTemp1.push(dataEveryThreeHours.main.temp_min);
-                    maxTemp1.push(dataEveryThreeHours.main.temp_max);
+                    temp1_arr.push(dataEveryThreeHours.main.temp);
                 }
                 else if  (dateNumber == today + 1){
-                    temp2.push(dataEveryThreeHours.main.temp);
-                    minTemp2.push(dataEveryThreeHours.main.temp_min);
-                    maxTemp2.push(dataEveryThreeHours.main.temp_max);
-
+                    temp2_arr.push(dataEveryThreeHours.main.temp);
                 }
                 else if  (dateNumber == today + 2){
-                    temp3.push(dataEveryThreeHours.main.temp);
-                    minTemp3.push(dataEveryThreeHours.main.temp_min);
-                    maxTemp3.push(dataEveryThreeHours.main.temp_max);
-
+                    temp3_arr.push(dataEveryThreeHours.main.temp);
                 }
                 else if  (dateNumber == today + 3){
-                    temp4.push(dataEveryThreeHours.main.temp);
-                    minTemp4.push(dataEveryThreeHours.main.temp_min);
-                    maxTemp4.push(dataEveryThreeHours.main.temp_max);
-
+                    temp4_arr.push(dataEveryThreeHours.main.temp);
                 }
                 else if  (dateNumber == today + 4){
-                    temp5.push(dataEveryThreeHours.main.temp);
-                    minTemp5.push(dataEveryThreeHours.main.temp_min);
-                    maxTemp5.push(dataEveryThreeHours.main.temp_max);
-
+                    temp5_arr.push(dataEveryThreeHours.main.temp);
                 }
                 else {}
-            })
+            });
 
+            var sum1 = temp1_arr.reduce(function (accumulator, currentValue) {
+                return accumulator + currentValue;
+            }, 0);
+            document.getElementById("temp1").innerText = (sum1 / (temp1_arr.length)).toFixed(1) + " °C";
+
+            var sum2 = temp2_arr.reduce(function (accumulator, currentValue) {
+                return accumulator + currentValue;
+            }, 0);
+            document.getElementById("temp2").innerText = (sum2 / (temp2_arr.length)).toFixed(1) + " °C";
+
+            var sum3 = temp3_arr.reduce(function (accumulator, currentValue) {
+                return accumulator + currentValue;
+            }, 0);
+            document.getElementById("temp3").innerText = (sum3 / (temp3_arr.length)).toFixed(1) + " °C";
+
+            var sum4 = temp4_arr.reduce(function (accumulator, currentValue) {
+                return accumulator + currentValue;
+            }, 0);
+            document.getElementById("temp4").innerText = (sum4 / (temp4_arr.length)).toFixed(1) + " °C";
+
+            var sum5 = temp5_arr.reduce(function (accumulator, currentValue) {
+                return accumulator + currentValue;
+            }, 0);
+            document.getElementById("temp5").innerText = (sum5 / (temp5_arr.length)).toFixed(1) + " °C";
 
 
         })
