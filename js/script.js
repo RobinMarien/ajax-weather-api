0b41bf60c4867b9d0254d796f35f0fae0601dc3d
RobinMarien <dev.robinmarien@gmail.com>
2019-09-10 15:30:54 +0200

    break

--- js/script.js
+++ js/script.js
@@ -52,8 +52,63 @@ document.getElementById("check").addEventListener("click", function(){
             var list = data.list;
 
             list.forEach(function(dataEveryThreeHours){
-                console.log(dataEveryThreeHours);
+
+                //console.log(dataEveryThreeHours);
+                var temp1 = [];
+                var minTemp1 = [];
+                var maxTemp1 = [];
+
+                var temp2 = [];
+                var minTemp2 = [];
+                var maxTemp2 = [];
+
+                var temp3 = [];
+                var minTemp3 = [];
+                var maxTemp3 = [];
+
+                var temp4 = [];
+                var minTemp4 = [];
+                var maxTemp4 = [];
+
+                var temp5 = [];
+                var minTemp5 = [];
+                var maxTemp5 = [];
+
+                var dateNumber = parseInt((dataEveryThreeHours.dt_txt).slice(8,10));
+                let today = new Date().getDate();
+
+                if (dateNumber == today){
+                    temp1.push(dataEveryThreeHours.main.temp);
+                    minTemp1.push(dataEveryThreeHours.main.temp_min);
+                    maxTemp1.push(dataEveryThreeHours.main.temp_max);
+                }
+                else if  (dateNumber == today + 1){
+                    temp2.push(dataEveryThreeHours.main.temp);
+                    minTemp2.push(dataEveryThreeHours.main.temp_min);
+                    maxTemp2.push(dataEveryThreeHours.main.temp_max);
+
+                }
+                else if  (dateNumber == today + 2){
+                    temp3.push(dataEveryThreeHours.main.temp);
+                    minTemp3.push(dataEveryThreeHours.main.temp_min);
+                    maxTemp3.push(dataEveryThreeHours.main.temp_max);
+
+                }
+                else if  (dateNumber == today + 3){
+                    temp4.push(dataEveryThreeHours.main.temp);
+                    minTemp4.push(dataEveryThreeHours.main.temp_min);
+                    maxTemp4.push(dataEveryThreeHours.main.temp_max);
+
+                }
+                else if  (dateNumber == today + 4){
+                    temp5.push(dataEveryThreeHours.main.temp);
+                    minTemp5.push(dataEveryThreeHours.main.temp_min);
+                    maxTemp5.push(dataEveryThreeHours.main.temp_max);
+
+                }
+                else {}
             })
+
 
 
         })
